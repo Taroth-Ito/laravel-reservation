e05c2a8d796f8966e6a1282dc060a123beec6ebb
Troth IT <taroth827@gmail.com>
2021-06-18 10:54:21 +0900

    一覧画面の表示修正

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -5,19 +5,27 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">
+                    <h3 class="text-center my-2">{{ __('Register User') }}</h3>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <p class="col-md-12 text-center"><span class="text-danger">(※)</span>は入力必須項目です。</p>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}<span
+                                    class="text-danger">(※)</span></label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
-                                    name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
+                                    name="name" value="{{ old('name') }}" required autocomplete="name" autofocus
+                                    placeholder="やんばる太朗">
+                                <small>Slack名を入力してください。</small>
                                 @error('name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -27,13 +35,32 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address')
-                                }}</label>
+                            <label for="term" class="col-md-4 col-form-label text-md-right">{{ __('Term') }}<span
+                                    class="text-danger">(※)</span></label>
+
+                            <div class="col-md-6">
+                                <input id="term" type="number" class="form-control @error('term') is-invalid @enderror"
+                                    name="term" value="{{ old('term') }}" required autocomplete="term" placeholder="3"
+                                    min="1">
+                                <small>半角数字2桁以内で入力してください。</small>
+                                @error('term')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}<span
+                                    class="text-danger">(※)</span></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                                    name="email" value="{{ old('email') }}" required autocomplete="email"
+                                    placeholder="****@mail.com">
+                                <small>今回は仮のメールアドレスを入力ください。</small>
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -44,12 +71,13 @@
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password')
-                                }}</label>
+                                }}<span class="text-danger">(※)</span></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password"
                                     class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="new-password">
+                                    required autocomplete="new-password" placeholder="********">
+                                <small>半角英数字8文字以上を入力してください。</small>
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -60,17 +88,19 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm
-                                Password') }}</label>
+                            <label for="password_confirm" class="col-md-4 col-form-label text-md-right">{{ __('Password
+                                Confirm') }}<span class="text-danger">(※)</span></label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control"
-                                    name="password_confirmation" required autocomplete="new-password">
+                                <input id="password_confirm" type="password" class="form-control"
+                                    name="password_confirmation" required autocomplete="new-password"
+                                    placeholder="********">
+                                <small>確認のためパスワードを再度入力してください。</small>
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-5 offset-md-5">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>

7bcf3359fc177caca5bf852ab65060083931fb8f
kia4545 <81552960+kia4545@users.noreply.github.com>
2021-06-23 14:47:32 +0900

    フォームタグの追加

--- src/resources/views/user/edit.blade.php
+++ src/resources/views/user/edit.blade.php
@@ -12,53 +12,54 @@
                 </div>
 
                 <div class="card-body">
-                    <div class="form-group row">
-                        <p class="col-md-12 text-center"><span class="text-danger">(※)</span>は入力必須項目です。</p>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="name" class="col-md-4 col-form-label text-md-right">名前<span
-                                class="text-danger">(※)</span>
-                        </label>
-                        <div class="col-md-6">
-                            <input id="name" class="form-control">
-                            <small>Slack名を入力してください。</small>
+                    <form>
+                        <div class="form-group row">
+                            <p class="col-md-12 text-center"><span class="text-danger">(※)</span>は入力必須項目です。</p>
                         </div>
-                    </div>
 
-                    <div class="form-group row">
-                        <label for="term" class="col-md-4 col-form-label text-md-right">期生<span
-                                class="text-danger">(※)</span>
-                        </label>
-
-                        <div class="col-md-6">
-                            <input id="term" class="form-control">
-                            <small>半角数字2桁以内で入力してください。</small>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">名前<span
+                                    class="text-danger">(※)</span>
+                            </label>
+                            <div class="col-md-6">
+                                <input id="name" class="form-control">
+                                <small>Slack名を入力してください。</small>
+                            </div>
                         </div>
-                    </div>
 
+                        <div class="form-group row">
+                            <label for="term" class="col-md-4 col-form-label text-md-right">期生<span
+                                    class="text-danger">(※)</span>
+                            </label>
 
-                    <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス<span
-                                class="text-danger">(※)</span></label>
-
-                        <div class="col-md-6">
-                            <input id="email" class="form-control">
-                            <small>今回は仮のメールアドレスを入力ください。</small>
+                            <div class="col-md-6">
+                                <input id="term" class="form-control">
+                                <small>半角数字2桁以内で入力してください。</small>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="row mt-4 align-items-center">
-                        <p class="col-md-4 text-md-right">パスワード</p>
-                        <p class="col-md-6">
-                            パスワードは変更できません
-                        </p>
-                    </div>
 
-                    <button class="btn btn-block btn-success col-md-4 mx-auto py-2 mt-5">
-                        更新する
-                    </button>
-                    <a class='btn btn-block btn-secondary text-white col-md-4 mx-auto py-2  mb-4'>戻る</a>
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス<span
+                                    class="text-danger">(※)</span></label>
+
+                            <div class="col-md-6">
+                                <input id="email" class="form-control">
+                                <small>今回は仮のメールアドレスを入力ください。</small>
+                            </div>
+                        </div>
+
+                        <div class="row mt-4 align-items-center">
+                            <p class="col-md-4 text-md-right">パスワード</p>
+                            <p class="col-md-6">
+                                パスワードは変更できません
+                            </p>
+                        </div>
+
+                        <button class="btn btn-block btn-success col-md-4 mx-auto py-2 mt-5">
+                            更新する
+                        </button>
+                        <a class='btn btn-block btn-secondary text-white col-md-4 mx-auto py-2  mb-4'>戻る</a>
                     </form>
                 </div>
             </div>
